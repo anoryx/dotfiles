934907d3fb9e7fe06ede6a1fd2741fa18d52b17c
anoryx <anoryx@anoryx.com>
2018-08-08 16:31:56 -0700

    added venv to prompt, fixed emacs conf

--- app/emacsd/init.el
+++ app/emacsd/init.el
@@ -7,16 +7,4 @@
 
 (customize-set-variable
   'vc-follow-symlinks t "follow symlinks w/o asking")
-(org-babel-load-file (locate-user-emacs-file "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(python-shell-interpreter "ipython"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file "~/.emacs.d/config.org")
