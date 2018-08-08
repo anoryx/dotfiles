9fa3d77d3d9f4896232461586e1cbb485a980e56
anoryx <anoryx@anoryx.com>
2018-08-08 13:44:30 -0700

    added emacs?

---
+++ app/emacsd/init.el
@@ -0,0 +1,22 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+
+(customize-set-variable
+  'vc-follow-symlinks t "follow symlinks w/o asking")
+(org-babel-load-file (locate-user-emacs-file "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(python-shell-interpreter "ipython"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
