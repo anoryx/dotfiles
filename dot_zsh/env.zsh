3db97515fa868510786be67017189a810fcca711
anoryx <anoryx@anoryx.com>
2024-10-10 13:35:07 -0700

    Add nvm to path

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -31,3 +31,9 @@ if [[ -d $LOCAL_BIN ]]; then
   export PATH="$LOCAL_BIN:$PATH"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+if [[ -d "$NVM_DIR" ]]
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
